d7c3750b4de783a084feb5f80a981ea82de73d49
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-21 16:38:07 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# Python list to string
+
+some_list = ['Bob', 'has', 'four', 'dogs'] # the base list
+
+list_to_string = ' '.join(some_list) # using the join() we can join the list into a string
+# the ' ' in the start is the way we want to connect the list
+# if we will do '/' the string will look like Bob/has/four/dogs
+
+print(list_ro_string) # Prints the string
+
+===============================================================
+# Output:
+
+>>> 'Bob has four dogs'
